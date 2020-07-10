ec64ae4a3ddb9113dc913a4e9d383b76544915fb
Riccardo <riccardo.andreatta@hotmail.com>
2020-07-10 22:13:49 +0100

    App component definitive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-    title = 'ConvertitoreTemperature';
-    tempF: number;
-    tempC: number;
-    tempK: number;
-
-    faranaithToCelsius(temp) {
-        return (temp - 32) / 1.8;
-    }
-
-    celsiusToFaranaith(temp) {
-        return (temp * 1.8) + 32;
-    }
-
-    celsiusToKelvin(temp) {
-        return temp + 237.36;
-    }
-
-    convertitore(temp, from, to) {
-        switch(from) {
-            case 'Celsius': {
-                switch (to) {
-                    case 'Celsius': {
-                        return temp;
-                    }
-                    case 'Fara': {
-                        return 1
-                    }
-                }
-                break;
-            }
-
-        }
-    }
-}
+export class AppComponent { }
