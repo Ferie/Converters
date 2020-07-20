02c062e7da4a7ac2e705b819e5ba80aca80066f7
Riccardo <riccardo.andreatta@hotmail.com>
2020-07-20 06:02:11 +0100

    Fixed project names

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
     it('should display welcome message', () => {
         page.navigateTo();
-        expect(page.getTitleText()).toEqual('ConvertitoreTemperature app is running!');
+        expect(page.getTitleText()).toEqual('Converters app is running!');
     });
 
     afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, './coverage/ConvertitoreTemperature'),
+            dir: require('path').join(__dirname, './coverage/converters'),
             reports: ['html', 'lcovonly', 'text-summary'],
             fixWebpackSourcePaths: true
         },
